2e7f5ef86da8009dadcd343a7471477e4d381609
WalissonCF <walissoncf44@gmail.com>
2023-05-29 01:03:27 -0300

    feat: adicionado lib Ace editor ao projeto

--- json-formatter/src/app/pages/pages.module.ts
+++ json-formatter/src/app/pages/pages.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ACE_CONFIG, AceConfigInterface, AceModule } from 'ngx-ace-wrapper';
 
-
+const DEFAULT_ACE_CONFIG: AceConfigInterface = {};
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    PagesRoutingModule,
     CommonModule,
-    PagesRoutingModule
+    AceModule,
+  ],
+  providers: [
+    {
+      provide: ACE_CONFIG,
+      useValue: DEFAULT_ACE_CONFIG
+    }
   ]
 })
 export class PagesModule { }
