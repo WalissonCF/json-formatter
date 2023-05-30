fe4db452bac3f9c26337be68a3ad66ff067e5635
WalissonCF <walissoncf44@gmail.com>
2023-05-30 19:59:08 -0300

    refactor: retirado rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
   }
 ];
