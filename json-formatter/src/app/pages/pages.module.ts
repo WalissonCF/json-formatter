1f8f5d0c715b65783d1d9a0e77edbb859c39e704
WalissonCF <walissoncf44@gmail.com>
2023-05-29 21:13:23 -0300

    feat: adicionada lib para copiar dados para area de transferencia do usuário

--- json-formatter/src/app/pages/pages.module.ts
+++ json-formatter/src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ACE_CONFIG, AceConfigInterface, AceModule } from 'ngx-ace-wrapper';
+import { ClipboardModule } from 'ngx-clipboard';
 
 const DEFAULT_ACE_CONFIG: AceConfigInterface = {};
 @NgModule({
@@ -14,6 +15,7 @@ const DEFAULT_ACE_CONFIG: AceConfigInterface = {};
   imports: [
     ReactiveFormsModule,
     PagesRoutingModule,
+    ClipboardModule,
     CommonModule,
     AceModule,
   ],
