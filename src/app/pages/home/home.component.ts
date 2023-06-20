04c2297b927e2e922b0b21f8694a2121a1a1f914
WalissonCF <walissoncf44@gmail.com>
2023-06-19 21:44:11 -0300

    feat: adição para conversão de json em arquivo typescript

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -48,10 +48,67 @@ export class HomeComponent {
       console.error('Erro ao formatar o JSON:', error);
     }
   }
-
-  public uploadFileJson(): void {
-
+  
+  public convertToJsonSchema(): void {
+    try {
+      const json = JSON.parse(this.jsonContent);
+      const interfaceContent = this.generateInterface(json);
+      this.downloadFile(interfaceContent, 'interface.ts');
+    } catch (error) {
+      console.error('Erro ao converter para a interface JSON:', error);
+    }
   }
+
+  private generateInterface(json: any): string {
+    const interfaceProperties = this.generateProperties(json);
+    return `interface Arquivo {\n${interfaceProperties}\n}`;
+  }
+
+  private generateProperties(obj: any, indent: number = 2): string {
+    const indentation = ' '.repeat(indent);
+    let properties = '';
+
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
+        const type = this.getPropertyType(value);
+
+        if (Array.isArray(value) && value.length > 0) {
+          const nestedProperties = this.generateProperties(value[0], indent + 2);
+          properties += `${indentation}${key}: {\n${nestedProperties}\n${indentation}};\n`;
+        } else if (typeof value === 'object' && value !== null) {
+          const nestedProperties = this.generateProperties(value, indent + 2);
+          properties += `${indentation}${key}: {\n${nestedProperties}\n${indentation}};\n`;
+        } else {
+          properties += `${indentation}${key}: ${type};\n`;
+        }
+      }
+    }
+
+    return properties;
+  }
+
+  private getPropertyType(value: any): string {
+    if (Array.isArray(value) && value.length > 0) {
+      return `${this.getPropertyType(value[0])}[]`;
+    } else {
+      return typeof value;
+    }
+  }
+
+  private downloadFile(content: string, filename: string): void {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+  
 
   public openModal(): void {
     this.isPopUpVisible = true;
